d051053ea3ce4c4241359c3c539a6ea34facff59
sam217pa <samuel.barreto8@gmail.com>
2016-05-11 19:41:08 +0200

    trying to adjust size of graphics

--- src/conversion_tract.R
+++ src/conversion_tract.R
@@ -6,20 +6,23 @@ library(cowplot)
 library(tikzDevice)
 library(extrafont)
 
-snp <- read_phruscle("../../seq_mars/data/phruscle_snpcall.csv")
+snp <- read_phruscle("../seq_mars/data/phruscle_snpcall.csv")
+height <- 6
+width <- 4
 
-pdf('../img/trace_w.pdf', family = "Ubuntu", height = 11.69, width = 8.27, onefile = FALSE)
-plot_align(snp, "w" ,plot_title = "Donneur AT") +
+pdf('img/trace_w.pdf', family = "Ubuntu", height = height, width = width, onefile = FALSE)
+plot_align(snp, "w" ,plot_title = "") +
     guides(color = FALSE, size = FALSE, shape = FALSE)
 dev.off()
-pdf('../img/trace_s.pdf', family = "Ubuntu", height = 11.69, width = 8.27, onefile = FALSE)
-plot_align(snp, "s" ,plot_title = "Donneur GC")+
+pdf('img/trace_s.pdf', family = "Ubuntu", height = height, width = width, onefile = FALSE)
+plot_align(snp, "s" ,plot_title = "")+
     guides(color = FALSE, size = FALSE, shape = FALSE)
 dev.off()
-pdf('../img/trace_ws.pdf', family = "Ubuntu", height = 11.69, width = 8.27, onefile = FALSE)
-plot_align(snp, "ws" ,plot_title = "Donneur AT / GC")+
+pdf('img/trace_ws.pdf', family = "Ubuntu", height = height, width = width, onefile = FALSE)
+plot_align(snp, "ws" ,plot_title = "")+
     guides(color = FALSE, size = FALSE, shape = FALSE)
 dev.off()
-pdf('../img/trace_sw.pdf', family = "Ubuntu", height = 11.69, width = 8.27, onefile = FALSE)
-   plot_align(snp, "sw" ,plot_title = "Donneur GC / AT")
+pdf('img/trace_sw.pdf', family = "Ubuntu", height = height, width = width, onefile = FALSE)
+   plot_align(snp, "sw" ,plot_title = "")+
+    guides(color = FALSE, size = FALSE, shape = FALSE)
 dev.off()
