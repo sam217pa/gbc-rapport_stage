c36ee2bc28d11c4bca08448b9df163e285007131
sam217pa <samuel.barreto8@gmail.com>
2016-05-18 21:29:50 +0200

    avec les simulations
    J'ai testé si l'écart entre le score de qualité aux sites restorés et
    aux sites non restorés était significatif.

--- src/neomutations.R
+++ src/neomutations.R
@@ -6,15 +6,38 @@ library(gcbiasr)
 library(dplyr)
 library(ggplot2)
 
+set_gcbiasr_theme()
 
-conversion_tract[
-    conversion_tract$refb == conversion_tract$snpb &
-    conversion_tract$refb != conversion_tract$expb,
-    ] %>%
-    keep_clean_only() %>%
-    group_by(inconv) %>%
-    summarise(mean = mean(qual))
+simulated_data = replicate(expr =
+              conversion_tract %>%
+              mutate(isrestor = ifelse(is.na(isrestor), FALSE, TRUE)) %>%
+              keep_clean_only() %>%
+              filter(cons == "x" | cons == "X") %>%
+              group_by(isrestor) %>%
+              sample_n(., 20, replace = FALSE) %>%
+              kruskal.test(data = ., qual ~ isrestor) %>%
+              broom::tidy(),
+          n = 100, simplify = FALSE
+          )
+
+# extraction de la p.value du test
+lapply(simulated_data, function(i) i$p.value) %>%
+    unlist() %>%
+    as.data.frame() %>%
+    mutate(signif = ifelse(. > 0.05, TRUE, FALSE)) %>%
+    summarise(100 - sum(signif))
+
+    qplot(data = ., .) +
+    geom_vline(xintercept = 0.05)
 
 conversion_tract %>%
-    group_by(inconv) %>%
-    summarise(mean = mean(qual))
+    keep_clean_only() %>%
+    filter(cons == "x" | cons == "X") %>%
+    group_by(isrestor) %>%
+    summarise(mean = mean(qual),  count = n(), median = median(qual))
+
+conversion_tract %>%
+    keep_clean_only() %>%
+    filter(cons == "x" | cons == "X") %>%
+    mutate(isrestor = ifelse(is.na(isrestor), FALSE, TRUE)) %>%
+    kruskal.test(data = . , qual ~ isrestor )
