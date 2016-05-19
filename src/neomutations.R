165255c2b67c7ce425d919efadef5d65ee604933
sam217pa <samuel.barreto8@gmail.com>
2016-05-19 15:30:04 +0200

    avant de modifier la partie statistique

--- src/neomutations.R
+++ src/neomutations.R
@@ -1,10 +1,13 @@
 ## -*- R -*-
 
-# calcul du score de qualité moyen aux néomutations
+## calcul du score de qualité moyen aux restaurations de l'haplotype sauvage.
 
 library(gcbiasr)
 library(dplyr)
 library(ggplot2)
+
+#' pas normal la distirbutino
+shapiro.test(sample(conversion_tract$qual, 1000))
 
 set_gcbiasr_theme()
 
@@ -41,3 +44,39 @@ conversion_tract %>%
     filter(cons == "x" | cons == "X") %>%
     mutate(isrestor = ifelse(is.na(isrestor), FALSE, TRUE)) %>%
     kruskal.test(data = . , qual ~ isrestor )
+
+conversion_tract %>%
+    keep_clean_only() %>%
+    mutate(badqual = ifelse(qual < 30, TRUE, FALSE),
+           isrestor = ifelse(is.na(isrestor), FALSE, TRUE)) %>%
+    group_by(isrestor) %>%
+    summarise(prop = mean(badqual), n())
+
+ggplot(conversion_tract,
+       aes(x = qual)) +
+    geom_histogram() +
+    facet_wrap( ~cons , scales = "free")
+
+conversion_tract %>%
+    group_by(name) %>%
+    summarise(med = median(qual)) %>%
+    qplot(data = ., med)
+
+
+##+ Quand on enlève les sites restaurés de faible qualité, on n'a pas de
+##+ différences significatives. Quand on les laisse, la différence est significative.
+
+wilcox.test(x = conversion_tract$qual[
+                conversion_tract$isrestor & conversion_tract$qual > 30
+            ],
+            y = conversion_tract$qual[
+                is.na(conversion_tract$isrestor)
+            ])
+
+## Différence significative
+wilcox.test(x = conversion_tract$qual[
+                conversion_tract$isrestor #& conversion_tract$qual > 30
+            ],
+            y = conversion_tract$qual[
+                is.na(conversion_tract$isrestor)
+            ])
