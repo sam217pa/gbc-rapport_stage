d7c06d155d238e8fde2f1b26159ff8a258875258
sam217pa <samuel.barreto8@gmail.com>
2016-05-20 13:24:54 +0200

    placé le graphique sur une seule page en paysage.

--- src/conversion_tract.R
+++ src/conversion_tract.R
@@ -24,3 +24,26 @@ pdf('img/trace_sw.pdf', family = "Ubuntu", height = height, width = width, onefi
    plot_align(snp, "sw" ,plot_title = "")+
     guides(color = FALSE, size = FALSE, shape = FALSE)
 dev.off()
+
+plot_all_4 <-
+    cowplot::plot_grid(
+        plot_align(snp, "s" ,plot_title = "") +
+        guides(color = FALSE, size = FALSE, shape = FALSE)
+       ,
+        plot_align(snp, "w" ,plot_title = "") +
+        guides(color = FALSE, size = FALSE, shape = FALSE)
+       ,
+        plot_align(snp, "ws" ,plot_title = "") +
+        guides(color = FALSE, size = FALSE, shape = FALSE)
+       ,
+        plot_align(snp, "sw" ,plot_title = "") +
+        guides(color = FALSE, size = FALSE, shape = FALSE)
+       ,
+        ncol = 4,
+        labels = c("a", "b", "c", "d")
+    )
+
+ggsave(file = "img/trace_4.pdf",
+       plot = plot_all_4,
+       device = "pdf",
+       width = 420, height = 297, units = "mm")
